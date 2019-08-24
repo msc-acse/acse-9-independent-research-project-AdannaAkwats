80d6fe99ad4cd606644e50cf7e4a69955a0ac5c5
AdannaAkwats <aa14415@ic.ac.uk>
2019-08-24 20:26:19 +0100

    replaced tests with big files

---
+++ ClimateModelling/integration_tests/Test_4D_user_function/example_function.py
@@ -0,0 +1,23 @@
+import iris
+
+"""
+This is an script that given an example of a user-given function
+In the command line or input file :
+file_name = example_function 
+func_name = temp_rh
+"""
+
+def temp_rh(cube):
+    """
+   Perform simple arithmetic with variables
+   :param data: a dictionary of variables and their data
+   :return: result of equation, name of results, unit
+   """
+
+    # Perform equation wirh temperature and salinity
+    result = cube['temp'].data - cube['rh'].data
+    name = 'diff_temp_rh'
+    long_name = 'difference of temperature and relative humidity'
+    unit = 'K'
+
+    return result, name, long_name, unit
